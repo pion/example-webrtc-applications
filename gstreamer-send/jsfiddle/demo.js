c641b530a001eb057d8b481185c50bf67d1931b4
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2019-05-14 08:58:29 +0100

    Fix gstreamer-send for Firefox temporarily
    Alter the transceiver direction to sendrecv to satisfy Firefox.

--- gstreamer-send/jsfiddle/demo.js
+++ gstreamer-send/jsfiddle/demo.js
@@ -28,9 +28,9 @@ pc.onicecandidate = event => {
 }
 
 // Offer to receive 1 audio, and 2 video tracks
-pc.addTransceiver('audio', {'direction': 'recvonly'})
-pc.addTransceiver('video', {'direction': 'recvonly'})
-pc.addTransceiver('video', {'direction': 'recvonly'})
+pc.addTransceiver('audio', {'direction': 'sendrecv'})
+pc.addTransceiver('video', {'direction': 'sendrecv'})
+pc.addTransceiver('video', {'direction': 'sendrecv'})
 pc.createOffer().then(d => pc.setLocalDescription(d)).catch(log)
 
 window.startSession = () => {
