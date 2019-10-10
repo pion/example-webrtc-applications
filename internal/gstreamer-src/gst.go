53cb08316ef365180aad2f6f69f3877b6c79dd6f
Sean DuBois <seaduboi@amazon.com>
2019-10-10 13:51:59 -0700

    Add support for PCM in GStreamer
    You can switch Opus to PCMU or PCMA and everything works!
    
    Relates to pion/webrtc#851

--- internal/gstreamer-src/gst.go
+++ internal/gstreamer-src/gst.go
@@ -26,25 +26,52 @@ type Pipeline struct {
 	tracks    []*webrtc.Track
 	id        int
 	codecName string
+	clockRate float32
 }
 
 var pipelines = make(map[int]*Pipeline)
 var pipelinesLock sync.Mutex
 
+const (
+	videoClockRate = 90000
+	audioClockRate = 48000
+	pcmClockRate   = 8000
+)
+
 // CreatePipeline creates a GStreamer Pipeline
 func CreatePipeline(codecName string, tracks []*webrtc.Track, pipelineSrc string) *Pipeline {
 	pipelineStr := "appsink name=appsink"
+	var clockRate float32
+
 	switch codecName {
 	case webrtc.VP8:
 		pipelineStr = pipelineSrc + " ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! " + pipelineStr
+		clockRate = videoClockRate
+
 	case webrtc.VP9:
 		pipelineStr = pipelineSrc + " ! vp9enc ! " + pipelineStr
+		clockRate = videoClockRate
+
 	case webrtc.H264:
 		pipelineStr = pipelineSrc + " ! video/x-raw,format=I420 ! x264enc bframes=0 speed-preset=veryfast key-int-max=60 ! video/x-h264,stream-format=byte-stream ! " + pipelineStr
+		clockRate = videoClockRate
+
 	case webrtc.Opus:
 		pipelineStr = pipelineSrc + " ! opusenc ! " + pipelineStr
+		clockRate = audioClockRate
+
 	case webrtc.G722:
 		pipelineStr = pipelineSrc + " ! avenc_g722 ! " + pipelineStr
+		clockRate = audioClockRate
+
+	case webrtc.PCMU:
+		pipelineStr = pipelineSrc + " ! audio/x-raw, rate=8000 ! mulawenc ! " + pipelineStr
+		clockRate = pcmClockRate
+
+	case webrtc.PCMA:
+		pipelineStr = pipelineSrc + " ! audio/x-raw, rate=8000 ! alawenc ! " + pipelineStr
+		clockRate = pcmClockRate
+
 	default:
 		panic("Unhandled codec " + codecName)
 	}
@@ -60,6 +87,7 @@ func CreatePipeline(codecName string, tracks []*webrtc.Track, pipelineSrc string
 		tracks:    tracks,
 		id:        len(pipelines),
 		codecName: codecName,
+		clockRate: clockRate,
 	}
 
 	pipelines[pipeline.id] = pipeline
@@ -76,11 +104,6 @@ func (p *Pipeline) Stop() {
 	C.gstreamer_send_stop_pipeline(p.Pipeline)
 }
 
-const (
-	videoClockRate = 90000
-	audioClockRate = 48000
-)
-
 //export goHandlePipelineBuffer
 func goHandlePipelineBuffer(buffer unsafe.Pointer, bufferLen C.int, duration C.int, pipelineID C.int) {
 	pipelinesLock.Lock()
@@ -88,12 +111,7 @@ func goHandlePipelineBuffer(buffer unsafe.Pointer, bufferLen C.int, duration C.i
 	pipelinesLock.Unlock()
 
 	if ok {
-		var samples uint32
-		if pipeline.codecName == webrtc.Opus {
-			samples = uint32(audioClockRate * (float32(duration) / 1000000000))
-		} else {
-			samples = uint32(videoClockRate * (float32(duration) / 1000000000))
-		}
+		samples := uint32(pipeline.clockRate * (float32(duration) / 1000000000))
 		for _, t := range pipeline.tracks {
 			if err := t.WriteSample(media.Sample{Data: C.GoBytes(buffer, bufferLen), Samples: samples}); err != nil {
 				panic(err)
