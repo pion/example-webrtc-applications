7a2c07ec17efe7d3c256d631764090417307c66f
Sebastian <sjeltsch@gmail.com>
2023-07-20 20:28:42 +0200

    Upgrade sfu-ws flutter example to v4
    Simplify and update flutter to work with null-safety

--- sfu-ws/flutter/lib/main.dart
+++ sfu-ws/flutter/lib/main.dart
@@ -3,24 +3,31 @@
 
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter_webrtc/flutter_webrtc.dart';
-import 'package:flutter/material.dart';
-import 'package:websocket/websocket.dart';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  _GetMyAppState createState() => _GetMyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _GetMyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
+  static const _textStyle = TextStyle(fontSize: 24);
+
   // Local media
   final _localRenderer = RTCVideoRenderer();
-  List _remoteRenderers = [];
+  List<RTCVideoRenderer> _remoteRenderers = [];
 
-  WebSocket _socket;
-  RTCPeerConnection _peerConnection;
+  WebSocketChannel? _socket;
+  late final RTCPeerConnection _peerConnection;
+
+  _MyAppState();
 
   @override
   void initState() {
@@ -32,7 +39,7 @@ class _GetMyAppState extends State<MyApp> {
     _peerConnection = await createPeerConnection({}, {});
 
     await _localRenderer.initialize();
-    var localStream = await navigator.mediaDevices
+    final localStream = await navigator.mediaDevices
         .getUserMedia({'audio': true, 'video': true});
     _localRenderer.srcObject = localStream;
 
@@ -41,14 +48,10 @@ class _GetMyAppState extends State<MyApp> {
     });
 
     _peerConnection.onIceCandidate = (candidate) {
-      if (candidate == null) {
-        return;
-      }
-
-      _socket.add(JsonEncoder().convert({
+      _socket?.sink.add(jsonEncode({
         "event": "candidate",
-        "data": JsonEncoder().convert({
-          'sdpMLineIndex': candidate.sdpMlineIndex,
+        "data": jsonEncode({
+          'sdpMLineIndex': candidate.sdpMLineIndex,
           'sdpMid': candidate.sdpMid,
           'candidate': candidate.candidate,
         })
@@ -57,29 +60,33 @@ class _GetMyAppState extends State<MyApp> {
 
     _peerConnection.onTrack = (event) async {
       if (event.track.kind == 'video' && event.streams.isNotEmpty) {
-	var renderer = RTCVideoRenderer();
-	await renderer.initialize();
-	renderer.srcObject = event.streams[0];
+        var renderer = RTCVideoRenderer();
+        await renderer.initialize();
+        renderer.srcObject = event.streams[0];
 
-	setState(() { _remoteRenderers.add(renderer); });
+        setState(() {
+          _remoteRenderers.add(renderer);
+        });
       }
     };
 
     _peerConnection.onRemoveStream = (stream) {
-      var rendererToRemove;
-      var newRenderList = [];
+      RTCVideoRenderer? rendererToRemove;
+      final newRenderList = <RTCVideoRenderer>[];
 
       // Filter existing renderers for the stream that has been stopped
-      _remoteRenderers.forEach((r) {
-        if (r.srcObject.id == stream.id) {
-		rendererToRemove = r;
-	} else {
-	  newRenderList.add(r);
-	}
-      });
+      for (final r in _remoteRenderers) {
+        if (r.srcObject?.id == stream.id) {
+          rendererToRemove = r;
+        } else {
+          newRenderList.add(r);
+        }
+      }
 
       // Set the new renderer list
-      setState(() { _remoteRenderers = newRenderList; });
+      setState(() {
+        _remoteRenderers = newRenderList;
+      });
 
       // Dispose the renderer we are done with
       if (rendererToRemove != null) {
@@ -87,18 +94,20 @@ class _GetMyAppState extends State<MyApp> {
       }
     };
 
-    _socket = await WebSocket.connect('ws://localhost:8080/websocket');
-    _socket.stream.listen((raw) async {
+    final socket =
+        WebSocketChannel.connect(Uri.parse('ws://localhost:8080/websocket'));
+    _socket = socket;
+    socket.stream.listen((raw) async {
       Map<String, dynamic> msg = jsonDecode(raw);
 
       switch (msg['event']) {
         case 'candidate':
-          Map<String, dynamic> parsed = jsonDecode(msg['data']);
+          final parsed = jsonDecode(msg['data']);
           _peerConnection
               .addCandidate(RTCIceCandidate(parsed['candidate'], '', 0));
           return;
         case 'offer':
-          Map<String, dynamic> offer = jsonDecode(msg['data']);
+          final offer = jsonDecode(msg['data']);
 
           // SetRemoteDescription and create answer
           await _peerConnection.setRemoteDescription(
@@ -107,10 +116,9 @@ class _GetMyAppState extends State<MyApp> {
           await _peerConnection.setLocalDescription(answer);
 
           // Send answer over WebSocket
-          _socket.add(JsonEncoder().convert({
+          _socket?.sink.add(jsonEncode({
             'event': 'answer',
-            'data':
-                JsonEncoder().convert({'type': answer.type, 'sdp': answer.sdp})
+            'data': jsonEncode({'type': answer.type, 'sdp': answer.sdp}),
           }));
           return;
       }
@@ -122,49 +130,35 @@ class _GetMyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'sfu-ws',
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('sfu-ws'),
+      title: 'sfu-ws',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('sfu-ws'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Local Video', style: _textStyle),
+            SizedBox(
+              width: 160,
+              height: 120,
+              child: RTCVideoView(_localRenderer, mirror: true),
             ),
-            body: OrientationBuilder(builder: (context, orientation) {
-              return Column(
-                children: [
-                  Row(
-                    children: [
-                      Text('Local Video', style: TextStyle(fontSize: 50.0))
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                          width: 160,
-                          height: 120,
-                          child: RTCVideoView(_localRenderer, mirror: true))
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text('Remote Video', style: TextStyle(fontSize: 50.0))
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      ..._remoteRenderers.map((remoteRenderer) {
-                        return SizedBox(
-                            width: 160,
-                            height: 120,
-                            child: RTCVideoView(remoteRenderer));
-                      }).toList(),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text('Logs Video', style: TextStyle(fontSize: 50.0))
-                    ],
-                  ),
-                ],
-              );
-            })));
+            const Text('Remote Video', style: _textStyle),
+            Row(
+              children: [
+                ..._remoteRenderers.map((remoteRenderer) {
+                  return SizedBox(
+                      width: 160,
+                      height: 120,
+                      child: RTCVideoView(remoteRenderer));
+                }).toList(),
+              ],
+            ),
+            const Text('Logs Video', style: _textStyle),
+          ],
+        ),
+      ),
+    );
   }
 }
