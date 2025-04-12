a576c655804d9c34dffb5205474b75d5c6e18918
Srayan Jana <valorzard.kiko@outlook.com>
2025-04-12 09:59:00 -0700

    fix license lint

--- ebiten-game/game/ui.go
+++ ebiten-game/game/ui.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024 The Ebitengine Authors
 
