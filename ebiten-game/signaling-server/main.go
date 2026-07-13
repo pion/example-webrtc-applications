8b8cbe4fd2196949e3d6a11f748a95cff424c773
Pion <59523206+pionbot@users.noreply.github.com>
2026-07-13 18:07:44 +0000

    Update CI configs to v0.12.4
    Update lint scripts and CI configs.

--- ebiten-game/signaling-server/main.go
+++ ebiten-game/signaling-server/main.go
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
+// Package main implements the signaling server for the Ebiten game example.
 package main
 
 import (
