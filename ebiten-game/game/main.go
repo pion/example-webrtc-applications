539e2aa0cbe9d46c3e91ff7a18fb91488ded5c88
Pion <59523206+pionbot@users.noreply.github.com>
2026-07-13 18:07:44 +0000

    Update CI configs to v0.12.4
    Update lint scripts and CI configs.

--- ebiten-game/game/main.go
+++ ebiten-game/game/main.go
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
+// Package main implements the networked Ebiten game client.
 package main
 
 //nolint:gci
